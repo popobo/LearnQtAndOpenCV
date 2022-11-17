1d1f8c1f9045e764b8425a84eb849c2c513bdd4f
popobo <331361996@qq.com>
2022-11-17 17:14:38 +0800

    use Qt render mat

---
+++ MatView.cpp
@@ -0,0 +1,6 @@
+#include "MatView.h"
+
+MatView::MatView()
+{
+
+}
---
+++ MatView.h
@@ -0,0 +1,12 @@
+#ifndef MATVIEW_H
+#define MATVIEW_H
+
+#include <QWidget>
+
+class MatView
+{
+public:
+    MatView();
+};
+
+#endif // MATVIEW_H
