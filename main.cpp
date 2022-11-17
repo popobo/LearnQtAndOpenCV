38e4e24afaafd7609b065c6ce1377675429ec448
popobo <331361996@qq.com>
2022-11-17 15:42:40 +0800

    遍历不连续的OpenCV Mat

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 #include <QApplication>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+void printMs(const std::string& text = "") {
+    static long long last = 0;
+    long long cur = getTickCount();
+    if (last == 0) {
+        last = cur;
+        return;
+    }
+    long long ms;
+    ms = ((double)(cur - last) / getTickFrequency()) * 1000;
+    if (text != "") {
+        std::cout << text << " " << ms << "ms" << std::endl;
+    }
+    last = getTickCount();
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,13 +29,21 @@ int main(int argc, char *argv[])
     //mat.create(3000, 4000, CV_8UC3);
     int es = mat.elemSize();
     int size = mat.rows * mat.cols * mat.elemSize();
-
+    printMs();
     for (int i = 0; i < size; i += es){
         mat.data[i] = 255;
         mat.data[i + 1] = 0;
         mat.data[i + 2] = 0;
     }
-
+    printMs(std::to_string(__LINE__));
+    for (int row = 0; row < mat.rows; row++) {
+        for (int col = 0; col < mat.cols; col++) {
+            (&mat.data[row * mat.step])[col*es] = 0;
+            (&mat.data[row * mat.step])[col*es+1] = 0;
+            (&mat.data[row * mat.step])[col*es+2] = 255;
+        }
+    }
+    printMs(std::to_string(__LINE__));
     namedWindow("mat");
     imshow("mat", mat);
     waitKey(0);
