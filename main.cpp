6d92c58f89135fce836ea587e704f7cf9c0b700e
popobo <331361996@qq.com>
2022-11-19 15:12:13 +0800

    test pyrDown and pyrUp

--- main.cpp
+++ main.cpp
@@ -22,42 +22,25 @@ void printMs(const std::string &text = "") {
     last = getTickCount();
 }
 
-Mat resizeImage(const Mat &src, int width, int height) {
-    Mat des(height, width, src.type());
-
-    int sx, sy = 0;
-    float dx = (float)src.cols / des.cols;
-    float dy = (float)src.rows / des.rows;
-    for (int r = 0; r < des.rows; r++) {
-        sy = r * dy + 0.5;
-        for (int c = 0; c < des.cols; c++) {
-            sx = c * dx + 0.5;
-            des.at<Vec3b>(r, c) = src.at<Vec3b>(sy, sx);
-        }
-    }
-    return des;
-}
-
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Widget w;
     w.show();
 
     Mat src = imread("1.png");
-    Mat des = resizeImage(src, 2000, 2000);
-    Mat des2;
-    resize(src, des2, Size(2000, 2000), 0, 0, INTER_NEAREST);
-
-    resize(src, des2, Size(1000, 1000), 0, 0, INTER_NEAREST);
-    namedWindow("des2");
-    imshow("des2", des2);
+    Mat gsrc;
+    pyrDown(src, gsrc);
+    Mat lsrc;
+    pyrUp(src, lsrc);
 
     namedWindow("src");
     imshow("src", src);
 
-    resize(src, des2, Size(1000, 1000), 0, 0, INTER_LINEAR);
-    namedWindow("des3");
-    imshow("des3", des2);
+    namedWindow("gsrc");
+    imshow("gsrc", gsrc);
+
+    namedWindow("lsrc");
+    imshow("lsrc", lsrc);
 
     waitKey(0);
     return a.exec();
