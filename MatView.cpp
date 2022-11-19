2d31a79a99840d809c63b20d9e15c5fef5baf7d9
popobo <331361996@qq.com>
2022-11-19 14:58:17 +0800

    test resize

--- MatView.cpp
+++ MatView.cpp
@@ -1,27 +1,12 @@
 #include "MatView.h"
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
 #include <QImage>
 #include <QPainter>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 using namespace cv;
 
-MatView::MatView(QWidget *widget):QOpenGLWidget(widget)
-{
-    m_mat = imread("1.png");
-    cvtColor(m_mat, m_mat, COLOR_BGR2RGB);
-    this->resize(QSize(m_mat.cols, m_mat.rows));
-}
+MatView::MatView(QWidget *widget) : QOpenGLWidget(widget) {}
 
-void MatView::paintEvent(QPaintEvent *e)
-{
-
-    //do not write business logic code
-    //only paint
-    std::cout << this->size().height() << " ," << this->size().width() << std::endl;
-    QImage img(m_mat.data, m_mat.cols, m_mat.rows, QImage::Format_RGB888);
-    QPainter painter;
-    painter.begin(this);
-    painter.drawImage(QPoint(0,0), img);
-}
+void MatView::paintEvent(QPaintEvent *e) {}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "widget.h"
 #include <QApplication>
+#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <iostream>
 
 using namespace cv;
 
-void printMs(const std::string& text = "") {
+void printMs(const std::string &text = "") {
     static long long last = 0;
     long long cur = getTickCount();
     if (last == 0) {
@@ -22,44 +22,42 @@ void printMs(const std::string& text = "") {
     last = getTickCount();
 }
 
-// a对比度1.0-3.0，b亮度0-100
-void changeGain(float a, int b, const Mat& src, Mat& des) {
+Mat resizeImage(const Mat &src, int width, int height) {
+    Mat des(height, width, src.type());
 
-    des.create(src.rows, src.cols, src.type());
-
-    for (int r=0;r<src.rows;r++){
-        for(int c=0;c<src.cols;c++){
-            for(int i=0;i<3;i++){
-                des.at<Vec3b>(r, c)[i]=saturate_cast<uchar>(a*src.at<Vec3b>(r,c)[i]+b);
-            }
+    int sx, sy = 0;
+    float dx = (float)src.cols / des.cols;
+    float dy = (float)src.rows / des.rows;
+    for (int r = 0; r < des.rows; r++) {
+        sy = r * dy + 0.5;
+        for (int c = 0; c < des.cols; c++) {
+            sx = c * dx + 0.5;
+            des.at<Vec3b>(r, c) = src.at<Vec3b>(sy, sx);
         }
     }
+    return des;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     Widget w;
     w.show();
 
     Mat src = imread("1.png");
-    Mat des;
-    printMs("1");
-    changeGain(1, 100, src, des);
-    printMs("2");
+    Mat des = resizeImage(src, 2000, 2000);
     Mat des2;
-    printMs("3");
-    src.convertTo(des2, -1, 1.0, 100);
-    printMs("4");
+    resize(src, des2, Size(2000, 2000), 0, 0, INTER_NEAREST);
+
+    resize(src, des2, Size(1000, 1000), 0, 0, INTER_NEAREST);
+    namedWindow("des2");
+    imshow("des2", des2);
 
     namedWindow("src");
     imshow("src", src);
 
-    namedWindow("des");
-    imshow("des", des);
-
-    namedWindow("des2");
-    imshow("des2", des2);
+    resize(src, des2, Size(1000, 1000), 0, 0, INTER_LINEAR);
+    namedWindow("des3");
+    imshow("des3", des2);
 
     waitKey(0);
     return a.exec();
